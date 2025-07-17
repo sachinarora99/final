a14b5131c58e95285cea45d45313bf1dd271ac4e
Sachin Arora <sachinarora99@gmail.com>
2025-07-17 19:00:38 +0530

    Updated MongoDB URI for deployment

--- leaderboard-backend/app.js
+++ leaderboard-backend/app.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/users');
+require('dotenv').config();
+
 
 dotenv.config();
 
